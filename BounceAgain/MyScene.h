1fe189a4df83792bf73fffa19dd485a29f64a9ed
Arvid Sigvardsson <arvid.sigvardsson@gmail.com>
2013-12-16 12:22:13 +0100

    Initial Commit

---
+++ BounceAgain/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  BounceAgain
+//
+
+//  Copyright (c) 2013 Arvid. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ BounceAgainTests/BounceAgainTests.m
@@ -0,0 +1,34 @@
+//
+//  BounceAgainTests.m
+//  BounceAgainTests
+//
+//  Created by Arvid on 2013-12-16.
+//  Copyright (c) 2013 Arvid. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BounceAgainTests : XCTestCase
+
+@end
+
+@implementation BounceAgainTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
